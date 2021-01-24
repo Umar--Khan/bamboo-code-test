ace8b32985a8356e980b3c45fe577803c9304e28
Umar--Khan <umarkhandev@gmail.com>
2021-01-24 21:43:43 +0000

    feat: added sequelize, added user models, further config

---
+++ server/api/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "password",
+    database: "bambank-test",
+    use_env_variables: 'DB_DEV_URL',
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "postgres",
+    password: "password",
+    database: "bambank-test",
+    use_env_variables: 'DB_DEV_URL',
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    username: "postgres",
+    password: "password",
+    database: "bambank-test",
+    use_env_variables: 'DB_DEV_URL',
+    host: "127.0.0.1",
+    dialect: "postgres"
+  }
+}
--- server/build/app.js
+++ server/build/app.js
@@ -13,5 +13,5 @@ app.use(_bodyParser["default"].urlencoded({
 app.use(_bodyParser["default"].json());
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
-  console.log("Server has started on port ".concat(PORT));
+  console.log("Server has started on port ".concat(port));
 });
